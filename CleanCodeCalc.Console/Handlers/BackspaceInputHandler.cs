dd565f8ebb0b7b698aa90a7af9624ee120b9bee1
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-07-03 16:14:18 +0200

    Fix bugs in BackspaceInputHandler

--- CleanCodeCalc.Console/Handlers/BackspaceInputHandler.cs
+++ CleanCodeCalc.Console/Handlers/BackspaceInputHandler.cs
@@ -1,4 +1,6 @@
 ﻿using CleanCodeCalc.Console.Models;
+using CleanCodeCalc.Library.Models;
+using CleanCodeCalc.Library.Utilities;
 
 namespace CleanCodeCalc.Console.Handlers;
 
@@ -9,8 +11,24 @@ public class BackspaceInputHandler : InputHandlerBase
         if (inputData.UserInput.Key == ConsoleKey.Backspace && !string.IsNullOrWhiteSpace(inputData.CalculationInput))
         {
             var newCalcInput = new Stack<char>(inputData.CalculationInput);
-            newCalcInput.Pop();
-            inputData.CalculationInput = new string(newCalcInput.ToArray());
+            var deleted = newCalcInput.Pop();
+
+            inputData.CalculationInput = new string(newCalcInput.Reverse().ToArray());
+
+            if (CalculationInput.TryCreate(inputData.CalculationInput, out var calcInput) && calcInput is not null)
+            {
+                inputData.Total = Calculator.Calculate(calcInput);
+            }
+
+            if (inputData.CalculationInput.Length == 1)
+            {
+                inputData.Total = double.Parse(inputData.CalculationInput);
+            }
+
+            if (inputData.CalculationInput.Length == 0)
+            {
+                inputData.Total = 0;
+            }
         }
         base.Handle(inputData);
     }
